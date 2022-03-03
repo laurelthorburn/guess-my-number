acbad8878779fc0eabc91e3dc5ccd312a94aa13b
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-03 15:56:30 -0700

    Approaching no input first

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value;
+// document.querySelector('.guess').value = 23;
+
+const x = function(){
+    console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = document.querySelector('.guess').value;
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    console.log(guess)
+
+    //check if there is a value
+    if (!guess) {
+        document.querySelector('.message').textContent = '❌ Please enter a guess';
+    }
+});
