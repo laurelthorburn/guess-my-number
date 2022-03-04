92908b075623716e89a13621c2ffc2fd199ce35a
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-04 12:50:07 -0700

    Adding number check logic

--- script.js
+++ script.js
@@ -6,18 +6,26 @@
 
 // document.querySelector('.guess').value = 23;
 
-const x = function(){
-    console.log(23);
-};
+
+//define secret number
+
+const secretNumber = Math.trunc(Math.random()*20)+1;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click',function(){
-    const guess = document.querySelector('.guess').value;
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
-
+    //wrap in Number() else it's a string, can console log type Of
+    const guess = Number(document.querySelector('.guess').value);
     console.log(guess)
 
     //check if there is a value
     if (!guess) {
         document.querySelector('.message').textContent = '❌ Please enter a guess';
-    }
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Just right!';
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too low 📉';
+    }  else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too high 📈';
+    } 
 });
