9ce5c38f64a5b0c1822ca836f142618bd72dc99c
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-04 13:01:19 -0700

    Created catch for when score is 0

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let score = 20;
 document.querySelector('.check').addEventListener('click',function(){
     //wrap in Number() else it's a string, can console log type Of
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess)
+    console.log(guess, typeof(guess))
 
     //check if there is a value
     if (!guess) {
@@ -24,14 +24,22 @@ document.querySelector('.check').addEventListener('click',function(){
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Just right!';
     } else if (guess < secretNumber) {
+        if (score > 1){
         document.querySelector('.message').textContent = 'Too low 📉';
         score--;
         document.querySelector('.score').textContent = score;
-
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game 😓';
+            document.querySelector('.score').textContent = 0;
+        }
     }  else if (guess > secretNumber) {
+        if (score > 1){
         document.querySelector('.message').textContent = 'Too high 📈';
         score--;
         document.querySelector('.score').textContent = score;
-
+    } else {
+        document.querySelector('.message').textContent = 'You lost the game 😓';
+        document.querySelector('.score').textContent = 0;
+    }
     } 
 });
