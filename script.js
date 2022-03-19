7efdd59c93cb6d9ad1ab043b4d944e90e43e7d17
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-19 06:22:31 -0600

    Added repeat game

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
 'use strict';
-
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.guess').value = 23;
-
+const playAgain = document.querySelector('.again');
 
 //define secret number
-
-const secretNumber = Math.trunc(Math.random()*20)+1;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.check').addEventListener('click',function(){
-    //wrap in Number() else it's a string, can console log type Of
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof(guess))
+document.querySelector('.check').addEventListener('click', function () {
+  //wrap in Number() else it's a string, can console log type Of
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof(guess))
 
-    //check if there is a value
-    if (!guess) {
-        document.querySelector('.message').textContent = '❌ Please enter a guess';
-        //condition: wins the game, yay
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Just right!';
-        document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = "30rem";
-    } else if (guess < secretNumber) {
-        if (score > 1){
-        document.querySelector('.message').textContent = 'Too low 📉';
-        score--;
-        document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game 😓';
-            document.querySelector('.score').textContent = 0;
-        }
-    }  else if (guess > secretNumber) {
-        if (score > 1){
-        document.querySelector('.message').textContent = 'Too high 📈';
-        score--;
-        document.querySelector('.score').textContent = score;
+  //check if there is a value
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ Please enter a guess';
+    //condition: wins the game, yay
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Just right!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
     } else {
-        document.querySelector('.message').textContent = 'You lost the game 😓';
-        document.querySelector('.score').textContent = 0;
+      document.querySelector('.message').textContent = 'You lost the game 😓';
+      document.querySelector('.score').textContent = 0;
     }
-    } 
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😓';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//let user play again
+
+// playAgain.addEventListener('click', function(){
+// 	window.location.reload();
+// })
+
+playAgain.addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
